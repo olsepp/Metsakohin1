9a10a16a484bcec632874df9089b5411a2e8e60b
Oliver Sepp <olsepp@taltech.ee>
2025-02-10 12:29:49 +0200

    env

--- verification.php
+++ verification.php
@@ -4,6 +4,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+require __DIR__ . '/vendor/autoload.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
