933fd22b544d911a240f0f14979eb348245abb71
virt51483 <virt51483@sn-69-25.tll07.zoneas.eu>
2025-02-11 08:00:40 +0000

    verification

--- verification.php
+++ verification.php
@@ -4,7 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
